89f74acaa0cdfbb40ceda4a1c02cbb1442c0faec
bakdoolot-kg <altybaevbaku15@gmail.com>
2022-10-25 22:20:26 +0600

    lesson 5 done

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// $('.list__item').click(function () {
+//     $(this).append("!")
+// })
+
+// scroll
+// dblclick
+// select
+// submit
+// focus
+// дагы башкалар
+
+// $('.db_click').dblclick(function () {
+//     $(this).append("!!!")
+// })
+
+
+
+// $("li").click(func);
+
+// function func () {
+//     $(this).append("!")
+//     $(this).unbind("click", func)
+// }
+
+
+// $("li").on("click mousemove", addWord)
+
+// function addWord() {
+    // alert("Копирование болбойт!")
+// }
+
+// $("li").one("click", addWord)
+
+
